53d06777b94b5cfc9eb9f47594039d434870c01f
jty <jty2762@naver.com>
2021-09-07 17:38:01 +0900

    java study

---
+++ practice2.java
@@ -0,0 +1,15 @@
+
+public class practice2 {
+
+	public static void main(String[] args) {
+		int x = 6, y = 3;
+		
+		System.out.println("x+y");
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+
+	}
+
+}
---
+++ practice3.java
@@ -0,0 +1,21 @@
+
+public class practice3 {
+
+	public static void main(String[] args) {
+		char ch = 'A';
+		int i ='A'; // 65Ãâ·Â
+		
+		String str1, str2, str3;
+		str1 = "ABCD";
+		str2 = "123";
+		str3 = str1 + str2; //ABCD123
+		
+		System.out.println(ch);
+		System.out.println(i);
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str3);
+			
+	}
+
+}
